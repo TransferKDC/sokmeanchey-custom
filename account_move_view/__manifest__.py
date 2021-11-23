58547d510d6b951e0dd5f336199d38917e59894f
jennysun1001 <sunchenny17@kit.edu.kh>
2021-11-23 18:00:30 +0700

    account move view

---
+++ account_move_view/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "account_move_view",
+    'summary': """ Add View Currency on List View""",
+    'description': """ Display Curency on View and Inovice for SMC""",
+    'author': "A2A Digital",
+    'category': 'Account',
+    'version': '14.0.1.0.0',
+    'depends': ['account'],
+    'data': [
+        'views/account_move_views.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'license': 'LGPL-3', 
+
+}
